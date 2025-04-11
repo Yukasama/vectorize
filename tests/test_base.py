98a9e92a3e2a0fa26d294c785cec3d1f89717ac2
Yukasama <yukasamaa@gmail.com>
2025-04-11 23:35:09 +0200

    docs: add use case diagrams, .editorconfig

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,4 @@
-"""
-Basic test module to verify that pytest is working correctly.
+"""Basic test module to verify that pytest is working correctly.
 Test naming convetions from pytest
 file: test_name
 func: test_function
@@ -7,7 +6,6 @@ func: test_function
 
 
 def test_base():
-    """
-    Test that always passes by asserting True is true.
+    """Test that always passes by asserting True is true.
     """
     assert True
