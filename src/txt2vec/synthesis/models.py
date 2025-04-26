af6f3e265288aaf4f90e234c2d86e9b00f982633
Yukasama <yukasamaa@gmail.com>
2025-04-27 00:36:33 +0200

    chore: improve models with syntax and descriptive attributes, fix circular import

--- src/txt2vec/synthesis/models.py
+++ src/txt2vec/synthesis/models.py
@@ -4,7 +4,7 @@ import uuid
 from datetime import datetime
 from typing import TYPE_CHECKING, Optional
 
-from sqlmodel import Field, Relationship, SQLModel, func
+from sqlmodel import Column, DateTime, Field, Relationship, SQLModel, func
 
 from txt2vec.common.models import ErrorInfo
 from txt2vec.common.status import Status
@@ -12,14 +12,24 @@ from txt2vec.common.status import Status
 if TYPE_CHECKING:
     from txt2vec.datasets.models import Dataset
 
+__all__ = ["Synthesis"]
+
 
 class Synthesis(SQLModel, table=True):
     """Synthetic generation model."""
 
+    __tablename__ = "synthesis"
+
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     """Unique identifier for the synthetic generation."""
 
-    start_date: datetime = Field(sa_column_kwargs={"server_default": func.now()})
+    start_time: datetime = Field(
+        default=None,
+        sa_column=Column(
+            DateTime(timezone=True),
+            insert_default=func.now(),
+        ),
+    )
     """Start time of the synthetic generation."""
 
     end_date: datetime | None = Field(default=None)
@@ -31,13 +41,14 @@ class Synthesis(SQLModel, table=True):
     error_id: uuid.UUID | None = Field(default=None, foreign_key="errorinfo.id")
     """Optional foreign key linking to a specific error."""
 
-    error: ErrorInfo | None = Relationship(
-        sa_relationship_kwargs={"back_populates": None}
-    )
+    error: ErrorInfo | None = Relationship(back_populates=None)
     """Relationship to the associated error information."""
 
-    status: Status = Field(sa_column_kwargs={"server_default": Status.RUNNING})
+    status: Status = Field(sa_column=Column(server_default=Status.RUNNING))
     """Current status of the synthetic generation."""
 
-    datasets: Optional["Dataset"] = Relationship(back_populates="synthesis")
+    generated_dataset: Optional["Dataset"] = Relationship(back_populates="synthesis")
     """Relationship to the associated datasets."""
+
+    deleted_at: datetime | None = Field(default=None)
+    """Optional timestamp for when the synthetic generation was deleted."""
