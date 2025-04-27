8d5602302c1a258df6a98a0e53ad96ba3a23d6f8
Yukasama <yukasamaa@gmail.com>
2025-04-28 01:22:26 +0200

    test: add locust script for loadtests

---
+++ scripts/locust.py
@@ -0,0 +1,36 @@
+"""Load testing with Locust."""
+
+from pathlib import Path
+
+from locust import HttpUser, constant_throughput, task
+
+from tests.datasets.utils import get_test_file
+
+
+class GetUser(HttpUser):
+    """Load tests for Dataset requests."""
+
+    host = "http://localhost:8000"
+    wait_time = constant_throughput(0.1)
+
+    @task(100)
+    def upload_dataset(self) -> None:
+        """Upload a dataset."""
+        for dataset_file in [
+            "default.csv",
+            "default.json",
+            "default.xml",
+            "default.xlsx",
+            "infer_fields.csv",
+            "custom_fields.csv",
+        ]:
+            file_path = (
+                Path(__file__).parent.parent
+                / "test_data"
+                / "datasets"
+                / "valid"
+                / dataset_file
+            )
+            files = get_test_file(file_path)
+            response = self.client.post("/v1/datasets", files=files)
+            print(f"{response.headers['Location'].split('/')[-1]}")
