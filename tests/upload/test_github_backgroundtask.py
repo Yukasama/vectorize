012b1a178ee4ff4e0cb881443cb573fde2d234c5
ManuelDausmann <156981587+domoar@users.noreply.github.com>
2025-05-17 09:10:41 +0200

    added test cases

--- tests/upload/test_github_backgroundtask.py
+++ tests/upload/test_github_backgroundtask.py
@@ -1,1 +1,24 @@
 """Tests for the GitHub backgroundtask."""
+
+
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_task_pending() -> None:
+    """Test for setting up PENDING TASK."""
+
+
+@pytest.mark.asyncio
+async def test_task_completed() -> None:
+    """Test for setting up COMPLETED TASK."""
+
+
+@pytest.mark.asyncio
+async def test_task_cancelled() -> None:
+    """Test for setting up CANCELLED TASK."""
+
+
+@pytest.mark.asyncio
+async def test_task_finished() -> None:
+    """Test for setting up FINISHED TASK."""
--- tests/upload/test_github_endpoint.py
+++ tests/upload/test_github_endpoint.py
@@ -1,1 +1,30 @@
 """Tests for the GitHub endpoint."""
+
+from __future__ import annotations
+
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_github_endpoint_valid() -> None:
+    """Test for github upload pipeline (VALID UPLOAD)."""
+
+
+@pytest.mark.asyncio
+async def test_github_endpoint_invalid() -> None:
+    """Test for github upload pipeline (INVALID UPLOAD)."""
+
+
+@pytest.mark.asyncio
+async def test_github_endpoint_error_caseone() -> None:
+    """Test for github upload pipeline errorhandling (VALID UPLOAD)."""
+
+
+@pytest.mark.asyncio
+async def test_github_endpoint_error_casetwo() -> None:
+    """Test for github upload pipeline errorhandling (VALID UPLOAD)."""
+
+
+@pytest.mark.asyncio
+async def test_github_endpoint_error_casethree() -> None:
+    """Test for github upload pipeline errorhandling (VALID UPLOAD)."""
