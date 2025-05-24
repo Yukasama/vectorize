741ea39b255f6d5ba0c2c827cd3a62ec24f816e4
Yukasama <yukasamaa@gmail.com>
2025-05-25 01:41:55 +0200

    fix: comment in ssl cert for server on dev

--- src/vectorize/server.py
+++ src/vectorize/server.py
@@ -1,5 +1,8 @@
 """ASGI server for the FastAPI application."""
 
+import os
+from pathlib import Path
+
 import uvicorn
 
 from vectorize.config import settings
@@ -9,13 +12,13 @@ __all__ = ["run"]
 
 def run() -> None:
     """Run the FastAPI application using Uvicorn."""
-    # base_dir = Path(
-    #     os.environ.get("CERT_DIR", Path(__file__).parent / "config" / "resources")
-    # )
+    base_dir = Path(
+        os.environ.get("CERT_DIR", Path(__file__).parent / "config" / "resources")
+    )
 
-    # ssl_keyfile = Path(os.environ.get("SSL_KEYFILE", str(base_dir / "key.pem")))
-    # ssl_certfile = Path(os.environ.get("SSL_CERTFILE", str(base_dir / "cert.pem")))
-    # ssl_enabled = ssl_keyfile.exists() and ssl_certfile.exists()
+    ssl_keyfile = Path(os.environ.get("SSL_KEYFILE", str(base_dir / "key.pem")))
+    ssl_certfile = Path(os.environ.get("SSL_CERTFILE", str(base_dir / "cert.pem")))
+    ssl_enabled = ssl_keyfile.exists() and ssl_certfile.exists()
 
     uvicorn.run(
         "vectorize:app",
@@ -25,6 +28,6 @@ def run() -> None:
         server_header=settings.server_header,
         log_config=None,
         log_level=None,
-        # ssl_keyfile=ssl_keyfile if ssl_enabled else None,
-        # ssl_certfile=ssl_certfile if ssl_enabled else None,
+        ssl_keyfile=ssl_keyfile if ssl_enabled else None,
+        ssl_certfile=ssl_certfile if ssl_enabled else None,
     )
