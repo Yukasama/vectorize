4f6e1328a120ab68927fc3e7e20d677a59157d10
ManuelDausmann <156981587+domoar@users.noreply.github.com>
2025-05-25 17:16:56 +0200

    fixed test case seeding

--- tests/ai_model/test_get_all_paged_valid.py
+++ tests/ai_model/test_get_all_paged_valid.py
@@ -8,68 +8,57 @@ import pytest
 from fastapi import status
 from fastapi.testclient import TestClient
 
-DEFAULT_PAGE_SIZE: int = 10
-DEFAULT_PAGE_NUMBER: int = 1
-
-# This should match whatever your seed function does
 SEEDED_MODEL_COUNT = 8
 
 
-@pytest.mark.asyncio
-def test_list_models_pagination_non_default_params(
-    client: TestClient,
-) -> None:
-    """Test pagination with non-default parameters, assuming DB is already seeded."""
-    last_pagenum = math.ceil(SEEDED_MODEL_COUNT / DEFAULT_PAGE_SIZE)
-    expected_last_page_items = SEEDED_MODEL_COUNT % DEFAULT_PAGE_SIZE or DEFAULT_PAGE_SIZE
+@pytest.mark.ai_model
+def test_list_models_pagination_size_10(client: TestClient) -> None:
+    """Test for non default page size."""
+    page_size = 10
+    page_number = 1
+    last_pagenum = math.ceil(SEEDED_MODEL_COUNT / page_size)
+    expected_last_page_items = SEEDED_MODEL_COUNT % page_size or page_size
 
-    # First page
-    route = f"/models?page={DEFAULT_PAGE_NUMBER}&size={DEFAULT_PAGE_SIZE}"
+    route = f"/models?page={page_number}&size={page_size}"
     resp = client.get(route)
     assert resp.status_code == status.HTTP_200_OK
     payload = resp.json()
-    assert payload["page"] == DEFAULT_PAGE_NUMBER
-    assert payload["size"] == DEFAULT_PAGE_SIZE
+    assert payload["page"] == page_number
+    assert payload["size"] == page_size
     assert payload["totalpages"] == last_pagenum
-    assert len(payload["items"]) == DEFAULT_PAGE_SIZE
+    assert len(payload["items"]) == SEEDED_MODEL_COUNT
 
     # Last page
-    route = f"/models?page={last_pagenum}&size={DEFAULT_PAGE_SIZE}"
+    route = f"/models?page={last_pagenum}&size={page_size}"
     resp = client.get(route)
     assert resp.status_code == status.HTTP_200_OK
     payload = resp.json()
     assert payload["page"] == last_pagenum
     assert len(payload["items"]) == expected_last_page_items
 
-    names = [item["name"] for item in payload["items"]]
-    assert names[0].startswith("Model_")
 
-
-@pytest.mark.asyncio
-def test_list_models_pagination_default_params(
-    client: TestClient,
-) -> None:
-    """Test pagination with default parameters, assuming DB is already seeded."""
-    last_pagenum = math.ceil(SEEDED_MODEL_COUNT / DEFAULT_PAGE_SIZE)
-    expected_last_page_items = SEEDED_MODEL_COUNT % DEFAULT_PAGE_SIZE or DEFAULT_PAGE_SIZE
+@pytest.mark.ai_model
+def test_list_models_pagination_size_5(client: TestClient) -> None:
+    """Test for default page size."""
+    page_size = 5
+    page_number = 1
+    last_pagenum = math.ceil(SEEDED_MODEL_COUNT / page_size)
+    expected_last_page_items = SEEDED_MODEL_COUNT % page_size or page_size
 
     # First page
-    route = f"/models?page={DEFAULT_PAGE_NUMBER}&size={DEFAULT_PAGE_SIZE}"
+    route = f"/models?page={page_number}&size={page_size}"
     resp = client.get(route)
     assert resp.status_code == status.HTTP_200_OK
     payload = resp.json()
-    assert payload["page"] == DEFAULT_PAGE_NUMBER
-    assert payload["size"] == DEFAULT_PAGE_SIZE
+    assert payload["page"] == page_number
+    assert payload["size"] == page_size
     assert payload["totalpages"] == last_pagenum
-    assert len(payload["items"]) == DEFAULT_PAGE_SIZE
+    assert len(payload["items"]) == page_size
 
     # Last page
-    route = f"/models?page={last_pagenum}&size={DEFAULT_PAGE_SIZE}"
+    route = f"/models?page={last_pagenum}&size={page_size}"
     resp = client.get(route)
     assert resp.status_code == status.HTTP_200_OK
     payload = resp.json()
     assert payload["page"] == last_pagenum
     assert len(payload["items"]) == expected_last_page_items
-
-    names = [item["name"] for item in payload["items"]]
-    assert names[0].startswith("Model_")
