1af2c729877ba4fa50b3e6849260ccde67d63aac
Yukasama <yukasamaa@gmail.com>
2025-06-29 11:33:22 +0200

    fix: tasks

--- src/vectorize/tasks.py
+++ src/vectorize/tasks.py
@@ -10,13 +10,16 @@ broker = RedisBroker(url=settings.dramatiq_redis_url)
 broker.add_middleware(AsyncIO())
 dramatiq.set_broker(broker)
 
+from vectorize.dataset.tasks import upload_hf_dataset_bg  # noqa: E402, I001
+from vectorize.evaluation.tasks import run_evaluation_bg  # noqa: E402
+from vectorize.training.tasks import run_training_bg  # noqa: E402
+
 from vectorize.ai_model.models import AIModel  # noqa: E402, F401
 from vectorize.dataset.models import Dataset  # noqa: E402, F401
 from vectorize.dataset.task_model import UploadDatasetTask  # noqa: E402, F401
-from vectorize.dataset.tasks import upload_hf_dataset_bg  # noqa: E402
-from vectorize.evaluation.tasks import run_evaluation_bg  # noqa: E402
 from vectorize.synthesis.models import SynthesisTask  # noqa: E402, F401
-from vectorize.training.tasks import run_training_bg  # noqa: E402
 from vectorize.upload.models import UploadTask  # noqa: E402, F401
+from vectorize.training.models import TrainingTask  # noqa: E402, F401
+from vectorize.evaluation.models import EvaluationTask  # noqa: E402, F401
 
 __all__ = ["run_evaluation_bg", "run_training_bg", "upload_hf_dataset_bg"]
